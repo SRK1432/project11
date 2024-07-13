eae6c63396f14e2033da299ea5c30ef61a1165b9
SRK1432 <kshrinivas309@gmail.com>
2024-07-13 10:47:20 +0530

    8

--- src/components/Pages/ExpenseForm.js
+++ src/components/Pages/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
@@ -7,20 +7,57 @@ const ExpenseForm = () =>{
     const [category, setCategory] = useState('');
     const [expenses, setExpenses] = useState([]);
 
-    const submitHandler=event=>{
+    const idToken = localStorage.getItem('idToken');
+    useEffect(() => {
+        const fetchExpenses = async () => {
+          try {
+            const response = await fetch(`https://expense-tracker-1c231-default-rtdb.firebaseio.com/expenses.json?auth=${idToken}`);
+            if (!response.ok) {
+              throw new Error('Failed to fetch expenses');
+            }
+        }
+        catch(error){
+            console.error(error);
+            }
+        };
+            fetchExpenses();
+          }, [idToken]);
+    const submitHandler= async(event)=>{
         event.preventDefault();
-        const newExpense = {amount,description,category};
-        setExpenses((prevExpenses)=>[...prevExpenses,newExpense])
-    }
+        const newExpense = { amount,description,category};
+        try {
+            const response = await fetch(`https://expense-tracker-1c231-default-rtdb.firebaseio.com/expenses.json?auth=${idToken}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(newExpense),
+            });
+      
+            if (!response.ok) {
+              throw new Error('Failed to add expense');
+            }
+      
+            const data = await response.json();
+            setExpenses(prevExpenses => [...prevExpenses,{ id: data.name, ...newExpense } ]);
+            setAmount('');
+            setDescription('');
+            setCategory('');
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        
+
     return(
         <>
         <form className="expense-form" onSubmit={submitHandler}>
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" onChange={((e)=>setAmount(e.target.value))}/>
+            <input type="number" id="amount" value={amount} onChange={((e)=>setAmount(e.target.value))}/>
             <label htmlFor="description">Description</label>
-            <input type="text" id="description" onChange={((e)=>setDescription(e.target.value))}/>
+            <input type="text" id="description" value={description} onChange={((e)=>setDescription(e.target.value))}/>
             <label htmlFor="category">Category</label>
-            <select onChange={((e)=>setCategory(e.target.value))}>
+            <select onChange={((e)=>setCategory(e.target.value))} value={category}>
             <option value=''>Select category</option>
             <option value='food'>Food</option>
             <option value='petrol'>Petrol</option>
@@ -39,4 +76,5 @@ const ExpenseForm = () =>{
         </>
     )
 }
+
 export default ExpenseForm;
