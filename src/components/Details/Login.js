8c87c821472875841adcb8d22e6d1ae76cfa2798
SRK1432 <kshrinivas309@gmail.com>
2025-09-06 11:54:10 +0530

    added change

--- src/components/Details/Login.js
+++ src/components/Details/Login.js
@@ -9,6 +9,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+
   
   const [loading, setLoading] = useState(false);
 
